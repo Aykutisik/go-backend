54ab098a219e49fff468b64ab3b25511cc683cb7
Aykut isik <aykut.isik@modanisa.com>
2022-03-01 12:17:14 +0300

    "CreateTodo Test is done"

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module Desktop/todo-backend/go-backend
 go 1.17
 
 require (
-	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.27.0
+	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.8.3
 )
 
@@ -12,16 +12,12 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/gofiber/utils v0.0.10 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
-	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/klauspost/compress v1.14.3 // indirect
-	github.com/mattn/go-colorable v0.1.7 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.33.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -30,8 +26,10 @@ require (
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,16 +1,21 @@
 package handler
 
 import (
+	"Desktop/todo-backend/go-backend/model"
 	"Desktop/todo-backend/go-backend/repository"
 	"Desktop/todo-backend/go-backend/service"
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"net/http/httptest"
-	"testing"
 )
 
 func TestGetTodoElements(t *testing.T) {
@@ -48,6 +53,48 @@ func TestGetTodoElements(t *testing.T) {
 			res, err := app.Test(req)
 			assert.Nil(t, err)
 			assert.Equal(t, 200, res.StatusCode)
+
+		})
+
+}
+
+func TestCreateTodo(t *testing.T) {
+
+	t.Run("CreateTodo should response with status code 200",
+		func(t *testing.T) {
+			mongoClient, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongoadmin:secret@localhost:27017/?authSource=admin"))
+			assert.Nil(t, err)
+
+			err = mongoClient.Connect(context.Background())
+			assert.Nil(t, err)
+
+			database := mongoClient.Database("todo_database")
+			collection := database.Collection("todo_list_elements")
+
+			repo := repository.NewRepository(database, mongoClient, collection)
+			service := service.NewService(repo)
+			handler := NewHandler(service)
+
+			app := fiber.New()
+
+			app.Post("/CreateTodo", handler.CreateTodo)
+			testBody := model.TodoElements{Text: "testText", Status: 0}
+
+			requestByte, _ := json.Marshal(testBody)
+			requestReader := bytes.NewReader(requestByte)
+			//err := json.NewEncoder(&buf).Encode(testBody)
+			//	body, err := ioutil.ReadAll(testBody)
+
+			// data := url.Values{}
+			// data.Add("text", `Lazy Test`). bytes.NewBufferString(data.Encode())
+			// data.Add("status", "0")
+
+			req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/CreateTodo"), requestReader)
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			res, err := app.Test(req)
+			assert.Nil(t, err)
+			fmt.Println(err)
+			assert.Equal(t, 201, res.StatusCode)
 
 		})
 
